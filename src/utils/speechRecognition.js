0ea07fd71cb305675fce31b33e668b755e31e7d4
LenAnderson <anderson.len@outlook.com>
2018-11-27 00:56:51 +0100

    check if SpeechRecognition class exists

--- src/utils/speechRecognition.js
+++ src/utils/speechRecognition.js
@@ -1,19 +1,21 @@
 import createNotification from "engine/createNotification";
 
-
-const rec = new (window.SpeechRecognition || window.webkitSpeechRecognition)();
-rec.maxAlternatives = 1;
-rec.addEventListener('end', rec.start.bind(rec));
-rec.start();
 let words = {};
-rec.addEventListener('result', (result) => {
-    let word = result.results[0][0].transcript.toLowerCase();
-    console.log('[speechRecognition]', result.results[0][0]);
-    createNotification(`You said: "${word}" (${Math.round(result.results[0][0].confidence*100)}%)`)
-    if (words[word]) {
-        words[word]();
-    }
-});
+let speechRecognitionClass = (window.SpeechRecognition || window.webkitSpeechRecognition);
+if (speechRecognitionClass) {
+    let rec = new speechRecognitionClass();
+    rec.maxAlternatives = 1;
+    rec.addEventListener('end', rec.start.bind(rec));
+    rec.start();
+    rec.addEventListener('result', (result) => {
+        let word = result.results[0][0].transcript.toLowerCase();
+        console.log('[speechRecognition]', result.results[0][0]);
+        createNotification(`You said: "${word}" (${Math.round(result.results[0][0].confidence*100)}%)`)
+        if (words[word]) {
+            words[word]();
+        }
+    });
+}
 
 export const listenFor = async (word, callback) => {
     word = word.toLowerCase();
